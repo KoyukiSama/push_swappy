bc6e9166adce29c59fcd7b04524a076ead79909a
Katarina Claes <katarinaclaes8@gmail.com>
2025-07-18 18:05:50 +0200

    ...

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,11 +6,21 @@
 /*   By: kclaes <kclaes@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/07/13 17:36:39 by kclaes        #+#    #+#                 */
-/*   Updated: 2025/07/18 16:50:18 by kclaes        ########   odam.nl         */
+/*   Updated: 2025/07/18 17:58:28 by kclaes        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ring_buffer.h"
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+// includes
+# include "ring_buffer.h"
+
+// return codes
+//  < 10 STX error
+# define ERR_STX_INIT 1
+// < 20 PARSE error
+# define ERR_BIG_STR_IS_VALID 10
 
 typedef struct s_stacks
 {
@@ -18,6 +28,12 @@ typedef struct s_stacks
 	t_ringbuff	rb_b;
 	int			error;
 }	t_stacks;
+
+typedef struct s_app
+{
+	t_stacks	stx;
+	char**		strs;
+}	t_app;
 
 // operations 
 // all returns success value
@@ -35,7 +51,11 @@ int			stx_rrb(t_stacks *stx);
 int			stx_rrr(t_stacks *stx);
 
 // stx management
-t_stacks	stx_init(size_t size);
+t_stacks	stx_init(size_t size, t_app *app);
 void		stx_destroy(t_stacks *stx);
+void		exit_clean(int err_code, t_app *app);
 
-// TODO fill stx
+// strs management
+void		strs_destroy(char **strs);
+
+#endif
--- src/push_swap_parse_bigstr.c
+++ src/push_swap_parse_bigstr.c
@@ -6,31 +6,53 @@
 /*   By: kclaes <kclaes@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/07/15 15:01:39 by kclaes        #+#    #+#                 */
-/*   Updated: 2025/07/18 17:01:03 by kclaes        ########   odam.nl         */
+/*   Updated: 2025/07/18 18:04:32 by kclaes        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
+#include "libft.h"
 #include "push_swap.h"
 
-static int	bigstr_is_valid(const char *str);
+static int	bigstr_is_valid(const char *str, t_app *app);
+static void	bigstr_fill_stx(const char *str, t_app *app);
 
-t_stacks stx_init_bigstr(const char *str)
+void	stx_init_bigstr(const char *str, t_app *app)
 {
 	size_t		i;
 	size_t		size;
 	t_stacks	stx;
 
-	size = bigstr_is_valid(str);
-	if (size == 0)
-		exit(1);
-	stx = stx_init(size);
-	if (stx.error == 1);
-		exit(2);
-	bigstr_fill_stx(stx);
+	size = bigstr_is_valid(str, app);
+	app->stx = stx_init(size, app);
+	bigstr_fill_stx(stx, str);
 }
 
-static void	bigstr_fill_stx(t_stacks *stx, const char *str)
+// returns ammount of nbr, returns 0 if invalid
+static int	bigstr_is_valid(const char *str, t_app *app)
+{
+	size_t	i;
+	size_t	nbr_count;
+
+	if (!ft_isdigit(str[0]))
+		exit_clean(ERR_BIG_STR_IS_VALID, app);
+	i = 0;
+	nbr_count = 1;
+	while (str[i])
+	{
+		while (ft_isdigit(str[i]))
+			i++;
+		if (str[i] == '\0')
+			break ;
+		if (!(str[i] == ' ' && ft_isdigit(str[i + 1])))
+			exit_clean(ERR_BIG_STR_IS_VALID, app);
+		nbr_count++;
+		i++;
+	}
+	return (nbr_count);
+}
+
+static void	bigstr_fill_stx(const char *str, t_app *app)
 {
 	size_t	i;
 	size_t	j;
@@ -40,34 +62,10 @@ static void	bigstr_fill_stx(t_stacks *stx, const char *str)
 	j = 0;
 	while (str[i])
 	{
-		while (is_nbr(str[i]))
+		while (ft_isdigit(str[i]))
 		{
 			
 			i++;
 		}
 	}
-}
-
-// returns ammount of nbr, returns 0 if invalid
-static int	bigstr_is_valid(const char *str)
-{
-	size_t	i;
-	size_t	nbr_count;
-
-	if (!is_nbr(str[0]))
-		return (0);
-	i = 0;
-	nbr_count = 1;
-	while (str[i])
-	{
-		while (is_nbr(str[i]))
-			i++;
-		if (str[i] == '\0')
-			break ;
-		if (!(str[i] == ' ' && is_nbr(str[i + 1])))
-			return (0);
-		nbr_count++;
-		i++;
-	}
-	return (nbr_count);
 }
