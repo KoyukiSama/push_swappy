2378295b25e757eb022279b269bddfff231856d9
Katarina Claes <katarinaclaes8@gmail.com>
2025-07-18 18:05:32 +0200

    ...

--- src/main.c
+++ src/main.c
@@ -6,11 +6,12 @@
 /*   By: kclaes <kclaes@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/07/13 18:17:40 by kclaes        #+#    #+#                 */
-/*   Updated: 2025/07/15 15:04:06 by kclaes        ########   odam.nl         */
+/*   Updated: 2025/07/18 17:35:47 by kclaes        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <push_swap.h>
 
 
 
@@ -18,10 +19,11 @@
 
 int main(int argc, char *argv[])
 {
+	static t_app	app = {NULL};
+
 	if (argc == 1)
 		return (0);
 	if (argc == 2)  // string given
-		
 	else			// nbr list given
 
 }
