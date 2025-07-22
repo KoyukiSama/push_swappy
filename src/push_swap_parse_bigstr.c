6e25ec7417dfe27bff78a1c3d7a57eeaac5fa144
Katarina Claes <katarinaclaes8@gmail.com>
2025-07-22 14:57:40 +0200

    fix (push_swap_parse_bigstr.c), [finish stx_init_bigstr, bigstr_is_valid, bigstr_fill_stx BUGS?]

--- src/push_swap_parse_bigstr.c
+++ src/push_swap_parse_bigstr.c
@@ -6,7 +6,7 @@
 /*   By: kclaes <kclaes@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/07/15 15:01:39 by kclaes        #+#    #+#                 */
-/*   Updated: 2025/07/19 16:13:12 by kclaes        ########   odam.nl         */
+/*   Updated: 2025/07/22 14:55:43 by kclaes        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,28 @@
 #include "libft.h"
 #include "push_swap.h"
 
-static int	bigstr_is_valid(const char *str, t_app *app);
-static void	bigstr_fill_stx(const char *str, t_app *app);
+static int	bigstr_is_valid(const char *str);
+static void	bigstr_fill_stx(const char *str, t_stacks *stx);
 
-void	stx_init_bigstr(const char *str, t_app *app)
+void	stx_init_bigstr(const char *str)
 {
 	size_t		i;
 	size_t		size;
 	t_stacks	stx;
 
-	size = bigstr_is_valid(str, app);
-	app->stx = stx_init(size, app);
-	bigstr_fill_stx(str, app);
+	size = bigstr_is_valid(str);
+	stx = stx_init(size);
+	bigstr_fill_stx(str, &stx);
 }
 
 // returns ammount of nbr, returns 0 if invalid
-static int	bigstr_is_valid(const char *str, t_app *app)
+static int	bigstr_is_valid(const char *str)
 {
 	size_t	i;
 	size_t	nbr_count;
 
 	if (!ft_isdigit(str[0]))
-		exit_clean(ERR_BIG_STR_IS_VALID, app);
+		exit_clean(ERR_BIG_STR_IS_VALID, NULL);
 	i = 0;
 	nbr_count = 1;
 	while (str[i])
@@ -45,26 +45,33 @@ static int	bigstr_is_valid(const char *str, t_app *app)
 		if (str[i] == '\0')
 			break ;
 		if (!(str[i] == ' ' && ft_isdigit(str[i + 1])))
-			exit_clean(ERR_BIG_STR_IS_VALID, app);
+			exit_clean(ERR_BIG_STR_IS_VALID, NULL);
 		nbr_count++;
 		i++;
 	}
 	return (nbr_count);
 }
 
-static void	bigstr_fill_stx(const char *str, t_app *app)
+// checks for int limit and fills stx
+static void	bigstr_fill_stx(const char *str, t_stacks *stx)
 {
 	size_t	i;
 	size_t	j;
-	int		nbr = 0;
+	int		nbr;
+	int		error;
 
 	i = 0;
 	j = 0;
 	while (str[i])
 	{
+		if (ft_isdigit(str[i]))
+			nbr = ft_atoi_err(str + i, &error);
+		if (error == 1)
+			exit_clean(ERR_BIG_STR_FILL_STX, stx);
+		rb_addbot(&(stx->rb_a), nbr);
 		while (ft_isdigit(str[i]))
-		{
 			i++;
-		}
+		while (ft_isspace(str[i]))
+			i++;
 	}
 }
