b686957514c3cc1f1448ba251e9a8d308b43833b
Katarina Claes <katarinaclaes8@gmail.com>
2025-07-22 19:02:56 +0200

    add (src/push_swap_parse_dupes.c), dupe check works

--- src/push_swap_parse_dupes.c
+++ src/push_swap_parse_dupes.c
@@ -6,13 +6,27 @@
 /*   By: kclaes <kclaes@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/07/22 18:37:40 by kclaes        #+#    #+#                 */
-/*   Updated: 2025/07/22 18:44:22 by kclaes        ########   odam.nl         */
+/*   Updated: 2025/07/22 18:58:37 by kclaes        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <push_swap.h>
+#include "push_swap.h"
 
 void	stx_no_dupes(t_stacks stx)
 {
-	while (stx.rb_a.count)
+	size_t	i;
+	size_t	j;
+
+	j = 0;
+	while (j < stx.rb_a.count)
+	{
+		i = j + 1;
+		while (i < stx.rb_a.count)
+		{
+			if (rb_get(stx.rb_a, j) == rb_get(stx.rb_a, i))
+				exit_clean(ERR_NO_DUPES, &stx);
+			i++;
+		}
+		j++;
+	}
 }
