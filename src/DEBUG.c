57bef5e64fdc820420fa09596e97054739c5d892
Katarina Claes <katarinaclaes8@gmail.com>
2025-07-27 19:26:50 +0200

    added colors to DEBUG.c

--- src/DEBUG.c
+++ src/DEBUG.c
@@ -1,18 +1,30 @@
 
 #include "push_swap.h"
 #include "ring_buffer.h"
+#include "colors.h"
 #include <stdio.h>
 
 void print_buff(t_stacks stx)
 {
 	t_ringbuff rb_a = stx.rb_a;
 	t_ringbuff rb_b = stx.rb_b;
+	int curr_val;
 	if (rb_a.buffer)
 	{
 		fprintf(stderr, "a: [ ");
 		for (size_t i = 0; i < rb_a.count; i++)
 		{
-			fprintf(stderr, "%i, ", rb_get(rb_a, i));
+			curr_val = rb_get(rb_a, i);
+			if (curr_val == stx.rba_low)
+				fprintf(stderr, COLOR_BRIGHT_GREEN"%i, "COLOR_RESET, curr_val);
+			else if (curr_val == stx.rba_high)
+				fprintf(stderr, COLOR_GREEN"%i, "COLOR_RESET, curr_val);
+			else if (curr_val == stx.rbb_low)
+				fprintf(stderr, COLOR_BRIGHT_BLUE"%i, "COLOR_RESET, curr_val);
+			else if (curr_val == stx.rbb_high)
+				fprintf(stderr, COLOR_BLUE"%i, "COLOR_RESET, curr_val);
+			else
+				fprintf(stderr, "%i, ", curr_val);
 		}
 		fprintf(stderr, " ]\n");
 	}
@@ -21,7 +33,17 @@ void print_buff(t_stacks stx)
 		fprintf(stderr, "b: [ ");
 		for (size_t i = 0; i < rb_b.count; i++)
 		{
-			fprintf(stderr, "%i, ", rb_get(rb_b, i));
+			curr_val = rb_get(rb_b, i);
+			if (curr_val == stx.rba_low)
+				fprintf(stderr, COLOR_BRIGHT_GREEN"%i, "COLOR_RESET, curr_val);
+			else if (curr_val == stx.rba_high)
+				fprintf(stderr, COLOR_GREEN"%i, "COLOR_RESET, curr_val);
+			else if (curr_val == stx.rbb_low)
+				fprintf(stderr, COLOR_BRIGHT_BLUE"%i, "COLOR_RESET, curr_val);
+			else if (curr_val == stx.rbb_high)
+				fprintf(stderr, COLOR_BLUE"%i, "COLOR_RESET, curr_val);
+			else
+				fprintf(stderr, "%i, ", curr_val);
 		}
 		fprintf(stderr, " ]\n");
 	}
